b26c4aa90f2c9050196716697366bec06fb3c64f
Jan <jkonieczny@pgs-soft.com>
2023-01-23 00:59:52 +0100

    fix

--- pages/empty.jsx
+++ pages/empty.jsx
@@ -5,11 +5,11 @@ import { useEffect, useRef, useState } from 'react';
 export default function Empty() {
   const [header, setHeader] = useState();
   useEffect(() => {
-    const params = new Proxy(new URLSearchParams(window.location.search), {
+    const params = new Proxy(new URLSearchParams(window?.location?.search), {
       get: (searchParams, prop) => searchParams.get(prop),
     });
     setHeader(params?.header);
-  }, [window.location.search]);
+  }, [window?.location?.search]);
 
   return (
     <Layout>
