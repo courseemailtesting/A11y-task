6086009a7e202e85ee16b943e044f43e46faaa6a
Jan <jkonieczny@pgs-soft.com>
2023-01-23 01:06:53 +0100

    fix2

--- pages/empty.jsx
+++ pages/empty.jsx
@@ -1,15 +1,23 @@
 import Layout from '@components/Layout';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Empty() {
   const [header, setHeader] = useState();
+  const [windowT, setWindowT] = useState();
+
+  if (typeof window !== 'undefined' && window?.location?.search !== windowT) {
+    setWindowT(window?.location?.search);
+  }
+
   useEffect(() => {
-    const params = new Proxy(new URLSearchParams(window?.location?.search), {
-      get: (searchParams, prop) => searchParams.get(prop),
-    });
-    setHeader(params?.header);
-  }, [window?.location?.search]);
+    if (windowT) {
+      const params = new Proxy(new URLSearchParams(window?.location?.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+      });
+      setHeader(params?.header);
+    }
+  }, [windowT]);
 
   return (
     <Layout>
